98035db70c873b3fe4dd4c87c25aa3b846bff574
Glitch (hello-express) <none>
2019-05-26 00:19:49 +0000

    👓🎋 Checkpoint ./client.js:97325/947

--- client.js
+++ client.js
@@ -4,38 +4,44 @@
 import Two from 'two.js';
 import createCamera from 'perspective-camera';
 
+const linkPoint = (point, point3d) => (camera) => {
+  const [x, y] = camera.project(point3d);
+  point.x = x;
+  point.y = y;
+};
+
 const two = new Two({
   width: 400,
   height: 400,
 }).appendTo(document.getElementById('main'));
 
 const camera = createCamera({
-  fov: 60,
+  fov: 30,
   near: 1,
   far: 10,
   viewport: [0, 0, two.width, two.height],
 });
 
-const start = [-5, 0, 0];
-const end = [5, 0, 0];
+const start = [-3, -1, 0];
+const end = [3, 1, 0];
 
-const line = two.makeLine(0, 0, 0, 0);
+const line = two.makePath(0, 0, 0, 0, 0, 0, true);
 line.stroke = 'black';
 line.linewidth = 2;
 
+const points = [
+  linkPoint(line.vertices[0], [-3, 1, 0]),
+  linkPoint(line.vertices[1], [0, -1, 0]),
+  linkPoint(line.vertices[2], [3, 1, 0]),
+];
+
 two.bind('update', () => {
-  const angle = parseFloat(document.getElementById('rotation').value);
-  camera.position = [Math.cos(angle)*5, 0, Math.sin(angle)*5];
+  const angle = parseFloat(document.getElementById('rotation').value) * Math.PI/180;
+  camera.position = [Math.sin(angle)*5, 0, Math.cos(angle)*5];
   camera.lookAt([0, 0, 0]);
   camera.update();
   
-  const [x1, y1] = camera.project(start);
-  line.vertices[0].x = x1;
-  line.vertices[0].y = y1;
-  
-  const [x2, y2] = camera.project(end);
-  line.vertices[1].x = x2;
-  line.vertices[1].y = y2;
+  points.forEach(point => point(camera));
 }).play();
 
 console.log('hello world :o');
