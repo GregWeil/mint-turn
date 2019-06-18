66039c5bc74fa3be35301e349088c379a0006bc8
Glitch (hello-express) <none>
2019-06-18 21:40:50 +0000

    ⏳🏥 Checkpoint ./make-anchor.js:97325/1379 ./client.js:97325/117

--- make-anchor.js
+++ make-anchor.js
@@ -1,11 +1,24 @@
 import { Anchor, Commands } from 'two.js';
 
 const makeAnchor = ([a, b, c]) => {
-  const anchor = new Anchor(0,0, 0,0, 0,0, Commands.curve);
+  const command = a.length ? Commands.curve : Commands.line;
+  const anchor = new Anchor(0,0, 0,0, 0,0, command);
   const updateAnchor = (camera) => {
-    const [x, y] = camera.project([a, b, c]);
-    anchor.x = x;
-    anchor.y = y;
+    if (command === Commands.curve) {
+      const [x, y] = camera.project(a);
+      anchor.x = x;
+      anchor.y = y;
+      const [lx, ly] = camera.project(b);
+      anchor.controls.left.x = lx - x;
+      anchor.controls.left.y = ly - y;
+      const [rx, ry] = camera.project(c);
+      anchor.controls.right.x = rx - x;
+      anchor.controls.right.y = ry - y;
+    } else {
+      const [x, y] = camera.project([a, b, c]);
+      anchor.x = x;
+      anchor.y = y;
+    }
   }
   return [anchor, updateAnchor];
 };
