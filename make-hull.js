949d8216ca3097b4da9e2340171cef452894b83b
Glitch (hello-express) <none>
2019-06-24 18:01:54 +0000

    🚟🛣 Checkpoint ./make-hull.js:97325/919

--- make-hull.js
+++ make-hull.js
@@ -15,6 +15,7 @@ const findOuterRoute = (sortedVertices) => {
     while (route.length > 1 && computeCrossProductZ(route[route.length-2], route[route.length-1], vertex) > 0) {
       route.pop();
     }
+    if (route.length === 0 || route[route.length-1][0] !== )
     route.push(vertex);
   }
   return route;
@@ -24,14 +25,17 @@ const makeHull = (vertices) => {
   const root = new Group();
   const update = (camera) => {
     root.remove(root.children);
-    const sortedVertices = vertices.map((vertex) => camera.project(vertex)).sort(([x1], [x2]) => x1 - x2);
+    const projectedVertices = vertices.map((vertex) => camera.project(vertex));
+    const sortedVertices = projectedVertices.sort(([x1, y1], [x2, y2]) => x1 !== x2 ? x1 - x2 : y1 - y2);
     const routeA = findOuterRoute(sortedVertices);
     sortedVertices.reverse();
     const routeB = findOuterRoute(sortedVertices);
+    window.routeA = [...routeA];
+    window.routeB = [...routeB];
     routeA.pop();
     routeB.pop();
     const route = [...routeA, ...routeB];
-    const path = new Path(route.map(([x, y]) => new Anchor(x, y, 0, 0, 0, 0)), true, false, false);
+    const path = new Path(route.map(([x, y]) => new Anchor(x,y, 0,0, 0,0)), true, false, false);
     root.add(path);
     root.add(vertices.map((vertex) => {
       const [x, y] = camera.project(vertex);
