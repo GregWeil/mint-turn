ff6e51d6e9dfab62769511cc0eba4fbc0475a1fb
Glitch (hello-express) <none>
2019-06-24 17:28:52 +0000

    💗🙎 Checkpoint ./make-hull.js:97325/666

--- make-hull.js
+++ make-hull.js
@@ -1,10 +1,19 @@
+// based on https://en.wikipedia.org/wiki/Graham_scan
+
 import { Ellipse, Group, Path } from 'two.js';
 import computeCentroid from './compute-centroid';
 
-const findOuterPath = (sortedVertices) => {
+const computeCrossProductZ = ([x1, y1], [x2, y2], [x3, y3]) => {
+  return (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+};
+
+const findOuterRoute = (sortedVertices) => {
   const route = [];
   for (let vertex in sortedVertices) {
-    
+    while (route.length > 1 && computeCrossProductZ(route[route.length-2], route[route.length-1], vertex) > 0) {
+      route.pop();
+    }
+    route.push(vertex);
   }
   return route;
 };
@@ -14,11 +23,13 @@ const makeHull = (vertices) => {
   const update = (camera) => {
     root.remove(root.children);
     const sortedVertices = vertices.sort(([x1], [x2]) => x2 - x1);
-    const routeA = findOuterPath(sortedVertices);
+    const routeA = findOuterRoute(sortedVertices);
     sortedVertices.reverse();
-    const routeB = findOuterPath(sortedVertices);
+    const routeB = findOuterRoute(sortedVertices);
+    routeA.pop();
+    routeB.pop();
     const route = [...routeA, ...routeB];
-    console.log(route);
+    window.route = route;
     root.add(vertices.map((vertex) => {
       const [x, y] = camera.project(vertex);
       return new Ellipse(x, y, 5, 5);
