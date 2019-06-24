e27660bdf8c453df8e703fff70cdbd13532ea918
Glitch (hello-express) <none>
2019-06-24 17:39:52 +0000

    🐠❄️ Checkpoint ./make-hull.js:97325/637

--- make-hull.js
+++ make-hull.js
@@ -2,6 +2,7 @@
 
 import { Ellipse, Group, Path } from 'two.js';
 import computeCentroid from './compute-centroid';
+import makeAnchor from './make-anchor';
 
 const computeCrossProductZ = ([x1, y1], [x2, y2], [x3, y3]) => {
   return (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
@@ -9,7 +10,8 @@ const computeCrossProductZ = ([x1, y1], [x2, y2], [x3, y3]) => {
 
 const findOuterRoute = (sortedVertices) => {
   const route = [];
-  for (let vertex in sortedVertices) {
+  for (let i = 0; i < sortedVertices.length; ++i) {
+    const vertex = sortedVertices[i];
     while (route.length > 1 && computeCrossProductZ(route[route.length-2], route[route.length-1], vertex) > 0) {
       route.pop();
     }
@@ -22,14 +24,15 @@ const makeHull = (vertices) => {
   const root = new Group();
   const update = (camera) => {
     root.remove(root.children);
-    const sortedVertices = vertices.sort(([x1], [x2]) => x2 - x1);
+    const sortedVertices = vertices.map((vertex) => camera.project(vertex)).sort(([x1], [x2]) => x1 - x2);
     const routeA = findOuterRoute(sortedVertices);
     sortedVertices.reverse();
     const routeB = findOuterRoute(sortedVertices);
     routeA.pop();
     routeB.pop();
     const route = [...routeA, ...routeB];
-    window.route = route;
+    const path = new Path(route.map(makeAnchor), true, false, false);
+    root.add(path);
     root.add(vertices.map((vertex) => {
       const [x, y] = camera.project(vertex);
       return new Ellipse(x, y, 5, 5);
