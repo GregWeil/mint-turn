3c0d5f9d06e081a721db9022cde37305d1c24260
Glitch (mint-turn) <none>
2019-07-08 19:27:28 +0000

    📻🎰 Checkpoint ./make-hull.js:97325/216 ./make-group.js:97325/264

--- make-group.js
+++ make-group.js
@@ -6,19 +6,12 @@ const computeDistance = ([x1, y1, z1], [x2, y2, z2]) => {
 
 const makeGroup = (vertex, children) => {
   const root = new Group();
-  const groupA = new Group();
-  const groupB = new Group();
-  root.add(groupA, groupB);
-  let flip = false;
   
   const update = (project, cameraPosition) => {
-    flip = !flip;
-    const group = flip ? groupA : groupB;
-    
     const childrenWithDepth = children.map(([child, , getCentroid]) => [child, computeDistance(cameraPosition, getCentroid())]);
     const depthSortedChildren = childrenWithDepth.sort(([, depthA], [, depthB]) => depthB - depthA);
     
-    group.add(depthSortedChildren.map(([child]) => child));
+    root.children = depthSortedChildren.map(([child]) => child);
     children.forEach(([, update]) => update(project, cameraPosition));
   };
   
