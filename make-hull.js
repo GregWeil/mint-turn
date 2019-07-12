346baafd2ad9572af38f8e9946d33d4461bacc5a
Glitch (mint-turn) <none>
2019-07-12 17:54:39 +0000

    🛳👡 Checkpoint ./make-hull.js:97325/196 ./example/client.js:97325/74

--- make-hull.js
+++ make-hull.js
@@ -26,8 +26,7 @@ const buildAngledPath = (vertices) => {
   return `M ${vertices.map(([x,y]) => `${x},${y}`).join(' L ')} Z`;
 };
 
-window.smoothness = 0.1;
-const buildCurvedPath = (vertices) => {
+const buildCurvedPath = (vertices, smoothing) => {
   let result = '';
   const prev = create();
   const offset = create();
@@ -41,8 +40,8 @@ const buildCurvedPath = (vertices) => {
     subtract(offset, next, prev);
     normalize(offset, offset);
     
-    scaleAndAdd(prev, current, offset, -distance(current, prev));
-    scaleAndAdd(next, current, offset, distance(current, next));
+    scaleAndAdd(prev, current, offset, -smoothing * distance(current, prev));
+    scaleAndAdd(next, current, offset, smoothing * distance(current, next));
     
     result += ` ${prev[0]},${prev[1]} ${current[0]},${current[1]}`;
     const start = ` C ${next[0]},${next[1]}`;
@@ -55,7 +54,7 @@ const buildCurvedPath = (vertices) => {
   return result;
 };
 
-const makeHull = (vertices, curved=true) => {
+const makeHull = (vertices, smoothing = 1/3) => {
   const path = makeElement('path');
   const update = (project) => {
     const projectedVertices = vertices.map((vertex) => project(vertex));
@@ -66,7 +65,7 @@ const makeHull = (vertices, curved=true) => {
     routeA.pop();
     routeB.pop();
     const route = [...routeA, ...routeB];
-    path.setAttribute('d', curved ? buildCurvedPath(route) : buildAngledPath(route));
+    path.setAttribute('d', smoothing ? buildCurvedPath(route, smoothing) : buildAngledPath(route));
   };
   const getCentroid = () => computeCentroid(vertices);
   return [path, update, getCentroid];
