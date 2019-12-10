54fd885ed8ea2a5c50606a98ba9897c63d6b9f4a
Glitch (mint-turn) <none>
2019-12-10 22:28:35 +0000

    🛬🚄 Checkpoint ./compute-curved-path.js:97325/1234

--- compute-curved-path.js
+++ compute-curved-path.js
@@ -8,28 +8,26 @@ const computeControlOffset = (offset, vertex, prev, next) => {
 };
 
 const computeCurvedPath = (vertices, smoothing, closed) => {
-  let result = '';
   const offset = create();
   const prev = create();
   const next = create();
   
-  for (let i = 0; i < vertices.length; ++i) {
-    copy(prev, vertices[(i+0) % vertices.length]);
-    const current = vertices[(i+1) % vertices.length];
-    copy(next, vertices[(i+2) % vertices.length]);
-    computeControlOffset(offset, current, prev, next);
+  return vertices.map((vertex, i) => {
+    copy(prev, vertices[(i+vertices.length-1) % vertices.length]);
+    copy(next, vertices[(i+vertices.length+1) % vertices.length]);
+    computeControlOffset(offset, vertex, prev, next);
     
-    scaleAndAdd(prev, current, offset, -smoothing);
-    scaleAndAdd(next, current, offset, smoothing);
+    scaleAndAdd(prev, vertex, offset, -smoothing);
+    scaleAndAdd(next, vertex, offset, smoothing);
     
-    if (!closed) {
-      if (i === vertices.length - 2) {
-        copy(prev, current);
-      }
-      if (i === vertices.length - 1) {
-        copy(next, current);
+    if (i === 0) {
+      if (!closed) {
+        
       }
     }
+    
+    return `${prev[0]},${prev[1]} ${current[0]},${current[1]} C ${next[0]},${next[1]}`;
+  }).join(' ');
     
     result += ` ${prev[0]},${prev[1]} ${current[0]},${current[1]}`;
     const start = ` C ${next[0]},${next[1]}`;
@@ -38,7 +36,8 @@ const computeCurvedPath = (vertices, smoothing, closed) => {
     } else {
       result = `M ${current[0]},${current[1]}` + start + result;
     }
-  }
+  });
+  
   return result;
 };
 
